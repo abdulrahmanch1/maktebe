ff648c3772c8ca76f1cf447cdd1b20ba26a7f475
AbdulrahmanChibon <abdoch292@gmail.com>
2025-07-21 16:32:04 +0300

    feat: setup react-router-dom and remove unused CRA files

---
+++ src/routes/index.js
@@ -0,0 +1,34 @@
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Import your page components here
+import HomePage from '../pages/HomePage';
+import ComplaintsPage from '../pages/ComplaintsPage';
+import SettingsPage from '../pages/SettingsPage';
+import BookDetailsPage from '../pages/BookDetailsPage';
+import AdminPage from '../pages/AdminPage';
+import LoginPage from '../pages/LoginPage';
+import RegisterPage from '../pages/RegisterPage';
+import FavoritesPage from '../pages/FavoritesPage';
+import ReadingListPage from '../pages/ReadingListPage';
+
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/complaints" element={<ComplaintsPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/book/:id" element={<BookDetailsPage />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="/reading-list" element={<ReadingListPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
